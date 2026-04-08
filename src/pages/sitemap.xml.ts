baf986250cf63aecbf5bc98e220837dbebf45b77
yeagoo <i90h@vip.qq.com>
2026-04-08 13:42:17 +0800

    Serve sitemap.xml as valid XML

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -2,7 +2,15 @@ import { resolveSiteUrl } from "../lib/site-config.js";
 
 export function GET() {
 	const siteUrl = resolveSiteUrl();
-	const targetUrl = new URL("/sitemap-index.xml", siteUrl).toString();
+	const sitemapUrl = new URL("/sitemap-0.xml", siteUrl).toString();
+	const xml = `<?xml version="1.0" encoding="UTF-8"?>` +
+		`<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">` +
+		`<sitemap><loc>${sitemapUrl}</loc></sitemap>` +
+		`</sitemapindex>`;
 
-	return Response.redirect(targetUrl, 301);
+	return new Response(xml, {
+		headers: {
+			"Content-Type": "application/xml; charset=utf-8",
+		},
+	});
 }
